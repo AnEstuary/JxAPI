aea9f3684b864e3d215d03dae031b723a9927ccd
Jason Lewis <jason@decomplecting.org>
2014-10-21 01:31:01 -0400

    Test the more things

--- src/test/java/com/yetanalytics/xapi/test/statement/StatementTest.java
+++ src/test/java/com/yetanalytics/xapi/test/statement/StatementTest.java
@@ -3,13 +3,19 @@ package com.yetanalytics.xapi.test.statement;
 import com.yetanalytics.xapi.statement.Statement;
 import org.junit.Test;
 
+import java.util.UUID;
+
 /**
  * Created by jason on 10/21/14.
  */
 public class StatementTest {
 
     @Test
-    public void StatementShouldNotBeNull(Statement stmt) {
+    public void StatementShouldBeBasicallyValid(Statement stmt) {
         assert(stmt != null);
+        assert(stmt.id.getClass() == UUID.class);
+        assert(stmt.actor != null);
+        assert(stmt.verb != null);
+        assert(stmt.object != null);
     }
 }
