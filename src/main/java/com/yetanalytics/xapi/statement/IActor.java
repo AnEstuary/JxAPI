851b645a3f7e18c12aa1bd7e313d7eb1bf14be0f
Jason Lewis <jason@decomplecting.org>
2014-10-22 01:05:38 -0400

    More docs

--- src/main/java/com/yetanalytics/xapi/statement/IActor.java
+++ src/main/java/com/yetanalytics/xapi/statement/IActor.java
@@ -2,6 +2,10 @@ package com.yetanalytics.xapi.statement;
 
 public interface IActor {
 
+    /**
+     *
+     * @return The object type of the implementing class, either 'Agent' or 'Group'
+     */
     public String objectType();
 
 }
--- src/main/java/com/yetanalytics/xapi/statement/IStatementObject.java
+++ src/main/java/com/yetanalytics/xapi/statement/IStatementObject.java
@@ -5,6 +5,9 @@ import java.net.URI;
 
 public interface IStatementObject {
 
-
+    /**
+     *
+     * @return the objectType of the implementing class, either Agent or Group.
+     */
     public String objectType();
 }
