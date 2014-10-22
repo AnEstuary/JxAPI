f2ae871509f905ccae54d71618018db1a0f15d3c
Jason Lewis <jason@decomplecting.org>
2014-10-22 03:56:10 -0400

    Commneting methods

--- src/main/java/com/yetanalytics/xapi/statement/Group.java
+++ src/main/java/com/yetanalytics/xapi/statement/Group.java
@@ -11,17 +11,32 @@ public class Group implements IActor, IStatementObject {
     public Agent[] member;
     public URI mbox;
 
-
+    /**
+     * CTOR for Group object
+     *
+     * @param nm        Param name, name of the group
+     * @param email     Email (mbox) URI identifier for Group
+     */
     public Group(String nm, URI email) {
         name = nm;
         mbox = email;
     }
 
+    /**
+     * CTOR for Group object
+     *
+     * @param nm        Param name, name of the group
+     * @param members   Array of Agent objects, members of the Group
+     */
     public Group(String nm, Agent[] members) {
         name = nm;
         member = members;
     }
 
+    /**
+     * Return objectType as required by IStatementObject and xAPI Spec
+     * @return  Returns "Group" as objectType
+     */
     @Override
     public String objectType() {
         return "Group";
