c86d8fa95044c22d886921e402a7266eb845c8b1
Jason Lewis <jason@decomplecting.org>
2014-10-22 03:50:01 -0400

    Add Group ctor's

--- src/main/java/com/yetanalytics/xapi/statement/Group.java
+++ src/main/java/com/yetanalytics/xapi/statement/Group.java
@@ -6,7 +6,21 @@ import java.net.URI;
  * Created by jason on 10/20/14.
  */
 public class Group implements IActor, IStatementObject {
+    public final String objectType = objectType();
+    public String name;
+    public Agent[] member;
+    public URI mbox;
 
+
+    public Group(String nm, URI email) {
+        name = nm;
+        mbox = email;
+    }
+
+    public Group(String nm, Agent[] members) {
+        name = nm;
+        member = members;
+    }
 
     @Override
     public String objectType() {
